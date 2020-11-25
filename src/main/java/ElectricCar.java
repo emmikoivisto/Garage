c3e253bd9fe5b0c1a6bc2e13d4f48d081e8afba3
emmikoivisto <emetsi@gmail.com>
2020-11-25 14:35:39 +0000

    electric car added

--- src/main/java/ElectricCar.java
+++ src/main/java/ElectricCar.java
@@ -1,2 +1,15 @@
+import Components.ElectricEngine;
+import Components.Tyres;
+
 public class ElectricCar {
+
+    private Tyres tyres;
+    private ElectricEngine electricEngine;
+
+    public ElectricCar(Tyres tyres, ElectricEngine electricEngine) {
+        this.tyres = tyres;
+        this.electricEngine = electricEngine;
+    }
+
+
 }
