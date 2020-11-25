bcf53cc02e6eb96f680ef3db4b1b9ba6292fc783
emmikoivisto <emetsi@gmail.com>
2020-11-25 15:31:36 +0000

    Vehicle class started

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,2 +1,45 @@
 public abstract class Vehicle {
+    private String model;
+    private String color;
+    private int price;
+    private int capacity;
+
+    public Vehicle(String model, String color, int price, int capacity){
+        this.model = model;
+        this.color = color;
+        this.price = price;
+        this.capacity = capacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
