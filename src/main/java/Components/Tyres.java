0cfd4f55dc923dce6ecf729c0d3f5143a5906b23
emmikoivisto <emetsi@gmail.com>
2020-11-25 14:17:32 +0000

    Tyres added

---
+++ src/main/java/Components/Tyres.java
@@ -0,0 +1,13 @@
+package Components;
+
+public class Tyres {
+
+    private String type;
+    private String size;
+
+    public Tyres(String type, String size){
+        this.type = type;
+        this.size = size;
+    };
+
+}
